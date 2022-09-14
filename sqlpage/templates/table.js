ebc4861cfa2d60eaed198b927097c595469a43ec
lovasoa <pere.jobs@gmail.com>
2022-09-14 12:58:41 +0400

    Re-integrate external js as static file

--- sqlpage/templates/table.js
+++
@@ -1,8 +0,0 @@
-// Tables
-for (const r of document.getElementsByClassName("data-list")) {
-    new List(r, {
-        valueNames: [...r.getElementsByClassName("sort")]
-            .map(t => t.dataset.sort),
-        indexAsync: true
-    });
-}
