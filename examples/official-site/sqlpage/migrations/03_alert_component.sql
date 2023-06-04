a1395f3575171c650072ebc7ae0419da54db3003
lovasoa <pere.jobs@gmail.com>
2023-06-04 12:45:37 +0200

    make alert component easier to use

--- examples/official-site/sqlpage/migrations/03_alert_component.sql
+++ examples/official-site/sqlpage/migrations/03_alert_component.sql
@@ -18,7 +18,7 @@ VALUES (
         'title',
         'Title of the alert message.',
         'TEXT',
-        FALSE,
+        TRUE,
         FALSE
     ),
     (
@@ -26,7 +26,7 @@ VALUES (
         'icon',
         'Icon name (from tabler-icons.io) to display next to the alert message.',
         'TEXT',
-        FALSE,
+        TRUE,
         TRUE
     ),
     (
@@ -34,7 +34,7 @@ VALUES (
         'color',
         'The color theme for the alert message.',
         'TEXT',
-        FALSE,
+        TRUE,
         TRUE
     ),
     (
@@ -42,7 +42,39 @@ VALUES (
         'description',
         'Detailed description or content of the alert message.',
         'TEXT',
-        FALSE,
+        TRUE,
+        TRUE
+    ),
+    (
+        'alert',
+        'dismissible',
+        'Whether the user can close the alert message.',
+        'TEXT',
+        TRUE,
+        TRUE
+    ),
+    (
+        'alert',
+        'important',
+        'Set this to TRUE to make the alert message more prominent.',
+        'BOOLEAN',
+        TRUE,
+        TRUE
+    ),
+    (
+        'alert',
+        'link',
+        'A URL to link to from the alert message.',
+        'URL',
+        TRUE,
+        TRUE
+    ),
+    (
+        'alert',
+        'link_text',
+        'Customize the text of the link in the alert message. The default is "Ok".',
+        'TEXT',
+        TRUE,
         TRUE
     ),
     (
@@ -55,69 +87,73 @@ VALUES (
     ),
     (
         'alert',
-        'dismissible',
-        'Whether the user can close the alert message.',
-        'TEXT',
-        FALSE,
-        TRUE
-    ),
-    (
-        'alert',
-        'important',
-        'Set this to TRUE to make the alert message more prominent.',
-        'BOOLEAN',
-        FALSE,
-        TRUE
-    ),
-    (
-        'alert',
-        'link_text',
+        'title',
         'Customize the text of the link in the alert message. The default is "Ok".',
         'TEXT',
         FALSE,
         TRUE
-    );
+    ),
+    (
+        'alert',
+        'color',
+        'Customize the color of the link.',
+        'TEXT',
+        FALSE,
+        TRUE
+    )
+;
 -- Insert example(s) for the component
 INSERT INTO example(component, description, properties)
 VALUES (
         'alert',
         'A basic alert message',
         JSON(
-            '[{"component":"alert"},{"title":"Attention","description":"This is an important message."}]'
+            '[{"component":"alert", "title":"Attention", "description":"This is an important message."}]'
         )
     ),
     (
         'alert',
         'A list of notifications',
         JSON(
-            '[{"component":"alert"},
-            {"title":"Success","description":"Item successfully added to your cart.", "icon":"check", "color": "green"},
-            {"title":"Warning","description":"Your cart is almost full.", "icon":"alert-triangle", "color": "yellow"},
-            {"title":"Error","description":"Your cart is full.", "icon":"alert-circle", "color": "red"}
+            '[
+            {"component" :"alert", "title":"Success","description":"Item successfully added to your cart.", "icon":"check", "color": "green"},
+            {"component":"alert", "title":"Warning","description":"Your cart is almost full.", "icon":"alert-triangle", "color": "yellow"},
+            {"component":"alert", "title":"Error","description":"Your cart is full.", "icon":"alert-circle", "color": "red"}
             ]'
         )
     ),
     (
         'alert',
-        'A full-featured notification message with a link',
+        'A full-featured notification message with multiple links',
         JSON(
-            '[{"component":"alert"},
+            '[
                 {
+                    "component" :"alert",
                     "title": "Your dashboard is ready!",
                     "icon":"analyze",
                     "color":"teal",
                     "dismissible": true,
-                    "description":"Your public web dashboard was successfully created.",
-                    "link":"/index.sql"
-                }]'
+                    "description":"Your public web dashboard was successfully created."
+                },
+                {
+                    "link":"dashboard.sql",
+                    "title": "View your dashboard"
+                },
+                { 
+                    "link" :"index.sql",
+                    "title": "Back to home",
+                    "color": "secondary"
+                }
+            ]'
         )
     ),
     (
         'alert',
         'An important danger alert message with an icon and color',
         JSON(
-            '[{"component":"alert"},
+            '[
             {
+                "component":"alert",
                 "title":"Alert",
                 "icon":"alert-circle",
                 "color":"red",
