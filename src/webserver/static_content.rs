6277f1a1370b1c532767d2455875d13cd40d4484
lovasoa <pere.jobs@gmail.com>
2023-06-14 00:19:00 +0200

    remove duplicated code and implement etag caching for static resources

--- src/webserver/static_content.rs
+++ src/webserver/static_content.rs
@@ -1,31 +1,36 @@
 use crate::utils::static_filename;
 use actix_web::{
-    http::header::{CacheControl, CacheDirective},
-    web, HttpResponse, Resource,
+    http::header::{CacheControl, CacheDirective, ETag, EntityTag, Header, IfNoneMatch},
+    web, HttpRequest, HttpResponse, Resource,
 };
 
+macro_rules! static_file_endpoint {
+    ($filestem:literal, $extension:literal) => {{
+        const FILENAME_WITH_TAG: &str = static_filename!(concat!($filestem, ".", $extension));
+        web::resource(FILENAME_WITH_TAG).to(|req: HttpRequest| async move {
+            let file_etag = EntityTag::new_strong(FILENAME_WITH_TAG.to_string());
+            if matches!(IfNoneMatch::parse(&req), Ok(IfNoneMatch::Items(etags)) if etags.iter().any(|etag| etag.weak_eq(&file_etag))) {
+                return HttpResponse::NotModified().finish();
+            }
+            HttpResponse::Ok()
+                .content_type(concat!("text/", $extension, ";charset=UTF-8"))
+                .insert_header(CacheControl(vec![
+                    CacheDirective::Public,
+                    CacheDirective::MaxAge(3600 * 24 * 7),
+                    CacheDirective::Extension("immutable".to_owned(), None),
+                ]))
+                .insert_header(ETag(file_etag))
+                .body(
+                    &include_bytes!(concat!(env!("OUT_DIR"), "/", $filestem, ".", $extension))[..],
+                )
+        })
+    }};
+}
+
 pub fn js() -> Resource {
-    web::resource(static_filename!("sqlpage.js")).to(|| async {
-        HttpResponse::Ok()
-            .content_type("text/javascript;charset=UTF-8")
-            .insert_header(CacheControl(vec![
-                CacheDirective::Public,
-                CacheDirective::MaxAge(7 * 24 * 3600),
-                CacheDirective::Extension("immutable".to_owned(), None),
-            ]))
-            .body(&include_bytes!(concat!(env!("OUT_DIR"), "/sqlpage.js"))[..])
-    })
+    static_file_endpoint!("sqlpage", "js")
 }
 
 pub fn css() -> Resource {
-    web::resource(static_filename!("sqlpage.css")).to(|| async {
-        HttpResponse::Ok()
-            .content_type("text/css;charset=UTF-8")
-            .insert_header(CacheControl(vec![
-                CacheDirective::Public,
-                CacheDirective::MaxAge(7 * 24 * 3600),
-                CacheDirective::Extension("immutable".to_owned(), None),
-            ]))
-            .body(&include_bytes!(concat!(env!("OUT_DIR"), "/sqlpage.css"))[..])
-    })
+    static_file_endpoint!("sqlpage", "css")
 }
