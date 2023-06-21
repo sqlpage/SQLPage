f8b13dd3ca37e7dcca1d8bde21159a65fd1fd5e4
lovasoa <pere.jobs@gmail.com>
2023-06-21 02:32:47 +0200

    add support for cookies

---
+++ examples/official-site/sqlpage/migrations/05_cookie.sql
@@ -0,0 +1,99 @@
+-- Insert the http_header component into the component table
+INSERT INTO component (name, description, icon)
+VALUES (
+        'cookie',
+        'Sets a cookie in the client browser, used for session management and storing user-related information.
+        
+        This component creates a single cookie. Since cookies need to be set before the response body is sent to the client,
+        this component should be placed at the top of the page, before any other components that generate output.
+        
+        After being set, a cookie can be accessed anywhere in your SQL code using the `sqlpage.cookie(''cookie_name'')` pseudo-function.',
+        'cookie'
+    );
+-- Insert the parameters for the http_header component into the parameter table
+INSERT INTO parameter (
+        component,
+        name,
+        description,
+        type,
+        top_level,
+        optional
+    )
+VALUES (
+        'cookie',
+        'name',
+        'The name of the cookie to set.',
+        'TEXT',
+        TRUE,
+        FALSE
+    ),
+    (
+        'cookie',
+        'value',
+        'The value of the cookie to set.',
+        'TEXT',
+        TRUE,
+        TRUE
+    ),
+    (
+        'cookie',
+        'path',
+        'The path for which the cookie will be sent. If not specified, the cookie will be sent for all paths.',
+        'TEXT',
+        TRUE,
+        TRUE
+    ),
+    (
+        'cookie',
+        'domain',
+        'The domain for which the cookie will be sent. If not specified, the cookie will be sent for all domains.',
+        'TEXT',
+        TRUE,
+        TRUE
+    ),
+    (
+        'cookie',
+        'secure',
+        'Whether the cookie should only be sent over a secure (HTTPS) connection. If not specified, the cookie will be sent over both secure and non-secure connections.',
+        'BOOLEAN',
+        TRUE,
+        TRUE
+    ),
+    (
+        'cookie',
+        'http_only',
+        'Whether the cookie should only be accessible via HTTP and not via client-side scripts. If not specified, the cookie will be accessible via both HTTP and client-side scripts.',
+        'BOOLEAN',
+        TRUE,
+        TRUE
+    ),
+    (
+        'cookie',
+        'remove',
+        'Set to TRUE to remove the cookie from the client browser. When specified, other parameters are ignored.',
+        'BOOLEAN',
+        TRUE,
+        TRUE
+    )
+    ;
+-- Insert an example usage of the http_header component into the example table
+INSERT INTO example (component, description, properties)
+VALUES (
+        'cookie',
+        'Create a cookie named `username` with the value `John Doe`...
+
+```sql
+SELECT ''cookie'' as component,
+        ''username'' as name,
+        ''John Doe'' as value;
+```
+
+and then display the value of the cookie:
+    
+```sql
+SELECT ''text'' as component,
+         ''Your name is '' || COALESCE(sqlpage.cookie(''username''), ''not known to us'');
+```
+        ',
+        JSON('[]')
+    );
---
+++ examples/read-and-set-http-cookies/README.md
@@ -0,0 +1,5 @@
+# SQLPage application with custom login in Postgres
+
+This is a very simple example of a website that uses the SQLPage web application framework. It uses a Postgres database for storing the data.
+
+It lets an user log in and out, and it shows a list of the users that have logged in.
