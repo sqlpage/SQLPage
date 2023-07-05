ed8e90d5be7c4cc84dba50d53322b38872939674
lovasoa <pere.jobs@gmail.com>
2023-07-05 18:22:05 +0200

    Add nested forms example

---
+++ examples/nested-forms/insert_user.sql
@@ -0,0 +1,5 @@
+INSERT INTO user (id, first_name, last_name, email) VALUES ($id, :"First name", :"Last name", :Email)
+ON CONFLICT (id) DO UPDATE SET first_name = excluded.first_name, last_name = excluded.last_name, email = excluded.email
+RETURNING
+    'http_header' AS component,
+    'edit_user.sql?id=' || id AS location; 
