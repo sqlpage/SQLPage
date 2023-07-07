2e62d110325359f62a987abf4ba8877ff7edeb6f
lovasoa <pere.jobs@gmail.com>
2023-07-07 20:07:25 +0200

    Implement the full user authentication flow in the example

--- examples/user-authentication/protected_page.sql
+++ examples/user-authentication/protected_page.sql
@@ -1,8 +1,6 @@
+SELECT raise_error('Invalid credentials, please log in') WHERE NOT is_valid_session(sqlpage.cookie('session'));
+
+SELECT 'shell' AS component, 'Protected page' AS title, 'lock' AS icon, '/' AS link, 'logout' AS menu_item;
 
 SELECT 'text' AS component,
         'This content is [top secret](https://youtu.be/dQw4w9WgXcQ). You cannot view it if you are not connected.' AS contents_md;
-
-SELECT EXISTS(SELECT 1 FROM login_session WHERE id=sqlpage.cookie('session')) AS contents;
-SELECT 'debug' AS component;
-SELECT * FROM login_session;
-SELECT sqlpage.cookie('session');
