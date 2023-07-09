44d1c03aaab3f8557de36e300729071513a80328
lovasoa <pere.jobs@gmail.com>
2023-07-09 12:12:49 +0200

    add an example illustrating how to integrate a dynamic react component

---
+++ examples/using react and other custom scripts and styles/my_react_component.js
@@ -0,0 +1,17 @@
+// Here we are using React and ReactDOM directly, but this file could be a compiled
+// version of a React component written in JSX.
+
+function RootComponent({ greeting_name }) {
+    const [count, setCount] = React.useState(0);
+    return React.createElement(
+        'button',
+        { onClick: () => setCount(count + 1), className: 'btn btn-primary' },
+        `Hello ${greeting_name}, you clicked me ${count} times!`
+    );
+}
+
+for (const container of document.getElementsByClassName('react_component')) {
+    const root = ReactDOM.createRoot(container);
+    const props = JSON.parse(container.dataset.props);
+    root.render(React.createElement(RootComponent, props, null));
+}
