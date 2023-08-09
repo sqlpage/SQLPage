630073b74a7d8ab88ae16516f60cf79e72b89e63
lovasoa <pere.jobs@gmail.com>
2023-08-09 19:36:52 +0200

    Improve authentication example
    On the main page, display 'logout' only if the user is logged in.
    Display 'login' and 'signup' only if they are not.
    
    See https://github.com/lovasoa/SQLpage/discussions/55

--- examples/user-authentication/index.sql
+++ examples/user-authentication/index.sql
@@ -1,9 +1,18 @@
+WITH logged_in AS (
+    SELECT COALESCE(sqlpage.cookie('session'), '') <> '' AS logged_in
+)
 SELECT 'shell' AS component,
     'User Management App' AS title,
     'user' AS icon,
     '/' AS link,
-    'signin' AS menu_item,
-    'signup' AS menu_item;
+    json_agg(menu_items.link) AS menu_item
+FROM (
+    SELECT 'signin' AS link FROM logged_in WHERE NOT logged_in
+    UNION ALL
+    SELECT 'signup' FROM logged_in WHERE NOT logged_in
+    UNION ALL
+    SELECT 'logout' FROM logged_in WHERE logged_in
+) AS menu_items;
 
 SELECT 'hero' AS component,
     'SQLPage Authentication Demo' AS title,
