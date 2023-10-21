7f17bd4151412ddeb685ef4050615b6ae3abd45a
ophir <pere.jobs@gmail.com>
2023-10-22 01:51:55 +0200

    use sqlpage.variables in the "variable number of fields" example

--- examples/forms with a variable number of fields/order_form.sql
+++ examples/forms with a variable number of fields/order_form.sql
@@ -7,16 +7,12 @@ SELECT 'form' as component,
 
 SELECT 'Name' as name, 'Your full name' AS placeholder;
 SELECT 'Email' as name, 'Your email address' AS placeholder;
-SELECT 'product_quantity[]' AS name,
-    'Quantity of "' || name || '"' AS label,
-    'Number of "' || name || '" you wish to order, for ' || price || ' € each.' AS description,
+SELECT 'product_quantity_' || id AS name,
+    'Quantity of ' || name || '' AS label,
+    'Number of ' || name || ' you wish to order, for ' || price || ' € each.' AS description,
     'number' AS type,
     1 AS step,
     0 as min,
     0 as value
 FROM products
 ORDER BY id;
-
--- We include the product ids in the form as hidden fields, so that we can use them for the insertion.
-SELECT 'product_id[]' AS name, '' AS label, 'hidden' AS type, id as value
-FROM products ORDER BY id;
