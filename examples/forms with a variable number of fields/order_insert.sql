164f54825ebc9d7e31f157507de4bb8f2b3dcfa3
ophir <pere.jobs@gmail.com>
2023-10-22 01:51:55 +0200

    use sqlpage.variables in the "variable number of fields" example

--- examples/forms with a variable number of fields/order_insert.sql
+++ examples/forms with a variable number of fields/order_insert.sql
@@ -3,12 +3,10 @@ VALUES (:Name, :Email);
 
 INSERT INTO order_items(order_id, quantity, product_id)
 SELECT
-    last_insert_rowid(), -- The id of the order we just inserted. Requires SQLPage v0.9.0 or later.
-    CAST(quantity.value AS INTEGER),
-    CAST(product.value AS INTEGER)
-FROM JSON_EACH(:product_quantity) quantity
-INNER JOIN JSON_EACH(:product_id) product USING (key)
-WHERE CAST(quantity.value AS INTEGER) > 0
-RETURNING
-    'orders.sql?id=' || order_id as link,
-    'redirect' as component;
+    last_insert_rowid() AS order_id, -- The id of the order we just inserted. Requires SQLPage v0.9.0 or later.
+    CAST(value AS INTEGER) AS quantity,
+    CAST(substr(key, length('product_quantity_') + 1) AS INTEGER) AS product_id -- extracts $id from product_quantity_$id
+FROM JSON_EACH(sqlpage.variables('post')) -- Iterates on all posted variables. Requires SQLPage v0.15.0 or later
+WHERE key LIKE 'product_quantity_%' -- variable names that start with 'product_quantity_' 
+   and CAST(value AS INTEGER) > 0
+RETURNING 'redirect' as component, 'orders.sql?id=' || order_id as link;
