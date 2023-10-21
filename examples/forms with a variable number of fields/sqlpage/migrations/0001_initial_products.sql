c3be66bab0a33a416bd41ee180632daca617c779
ophir <pere.jobs@gmail.com>
2023-10-22 01:16:56 +0200

    add products in the example

---
+++ examples/forms with a variable number of fields/sqlpage/migrations/0001_initial_products.sql
@@ -0,0 +1,6 @@
+-- Insert statements to populate the 'products' table with unusual products
+INSERT INTO products (name, price) VALUES ('Invisible Umbrella', 99.99);
+INSERT INTO products (name, price) VALUES ('Silent Alarm Clock', 49.95);
+INSERT INTO products (name, price) VALUES ('Pet Rock 2.0', 19.99);
+INSERT INTO products (name, price) VALUES ('Chocolate Teapot', 39.99);
+INSERT INTO products (name, price) VALUES ('Square Wheels for Your Bicycle', 29.99);
