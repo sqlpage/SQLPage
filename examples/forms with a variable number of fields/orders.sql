fc62c38ff4b826fdf568b7cd2cca088d7806ea6c
ophir <pere.jobs@gmail.com>
2023-10-22 15:51:26 +0200

    improve example

--- examples/forms with a variable number of fields/orders.sql
+++ examples/forms with a variable number of fields/orders.sql
@@ -1,8 +1,12 @@
 SELECT 'alert' as component,
-    'Thanks !' as title,
+    format('Thanks %s!', customer_name) as title,
     'analyze' as icon,
     'teal' as color,
-    'Your order is being processed. You will hear from us soon.' as description;
+    format('Your order is being processed.
+        You will shortly receive an email on %s with a receipt.',
+    customer_email) as description
+from orders where id = $id;
+
 SELECT 'index.sql' as link,
     'Back to homepage' as title;
 
