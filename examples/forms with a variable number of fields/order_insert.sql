91def50180a00d6ceecc0ed17e99463f4c25926c
ophir <pere.jobs@gmail.com>
2023-10-22 15:41:33 +0200

    form example: use raw product ids  for clearer code

--- examples/forms with a variable number of fields/order_insert.sql
+++ examples/forms with a variable number of fields/order_insert.sql
@@ -5,8 +5,8 @@ INSERT INTO order_items(order_id, quantity, product_id)
 SELECT
     last_insert_rowid() AS order_id, -- The id of the order we just inserted. Requires SQLPage v0.9.0 or later.
     CAST(value AS INTEGER) AS quantity,
-    CAST(substr(key, length('product_quantity_') + 1) AS INTEGER) AS product_id -- extracts $id from product_quantity_$id
+    CAST(key AS INTEGER) AS product_id -- extracts converts the field name into a number
 FROM JSON_EACH(sqlpage.variables('post')) -- Iterates on all posted variables. Requires SQLPage v0.15.0 or later
-WHERE key LIKE 'product_quantity_%' -- variable names that start with 'product_quantity_' 
+WHERE product_id > 0 -- we used the product id as a variable name in the product quatntity form fields 
    and CAST(value AS INTEGER) > 0
 RETURNING 'redirect' as component, 'orders.sql?id=' || order_id as link;
