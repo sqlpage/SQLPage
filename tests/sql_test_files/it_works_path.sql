ee2a1f071e027aeee02b8bf7af2d8a490fc7a441
lovasoa <pere.jobs@gmail.com>
2023-10-26 03:00:23 +0200

    fix test error msg

--- tests/sql_test_files/it_works_path.sql
+++ tests/sql_test_files/it_works_path.sql
@@ -1,6 +1,6 @@
 select 'text' as component, 
     CASE sqlpage.path()
         WHEN '/tests/sql_test_files/it_works_path.sql' THEN 'It works !'
-        ELSE 'It failed ! Expected %2F, got ' || sqlpage.path() || '.'
+        ELSE 'It failed ! Expected "/tests/sql_test_files/it_works_path.sql", got "' || sqlpage.path() || '"".'
     END
     AS contents;
