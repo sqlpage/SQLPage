c5a56937b459ca463e4ade9b76ee301b4827ee46
lovasoa <pere.jobs@gmail.com>
2023-11-06 23:22:33 +0100

    add an explanation about the logged_in_user function
    Hopefully it prevents users from introducing security vulnerabilities in their websites

--- examples/user-authentication/protected_page.sql
+++ examples/user-authentication/protected_page.sql
@@ -1,6 +1,8 @@
 SELECT 'redirect' AS component,
         'signin.sql?error' AS link
 WHERE logged_in_user(sqlpage.cookie('session')) IS NULL;
+-- logged_in_user is a custom postgres function defined in the first migration of this example
+-- that avoids having to repeat `(SELECT username FROM login_session WHERE id = session_id)` everywhere. 
 
 SELECT 'shell' AS component, 'Protected page' AS title, 'lock' AS icon, '/' AS link, 'logout' AS menu_item;
 
