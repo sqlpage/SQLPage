46136154eb0d613d318ea5a999497c9ba1296c88
lovasoa <pere.jobs@gmail.com>
2023-11-07 00:01:20 +0100

    simplify the user authentication example and make it easier to adapt to different databases

--- examples/user-authentication/create_user.sql
+++ examples/user-authentication/create_user.sql
@@ -1,21 +1,10 @@
-WITH inserted_user AS (
-    INSERT INTO user_info (username, password_hash)
-    VALUES (:username, sqlpage.hash_password(:password))
-    ON CONFLICT (username) DO NOTHING
-    RETURNING username
-)
-SELECT 'hero' AS component,
-    'Welcome' AS title,
-    'Welcome, ' || username || '! Your user account was successfully created. You can now log in.' AS description,
-    'https://upload.wikimedia.org/wikipedia/commons/thumb/e/e1/Community_wp20.png/974px-Community_wp20.png' AS image,
-    'signin.sql' AS link,
-    'Log in' AS link_text
-FROM inserted_user
-UNION ALL
-SELECT 'hero' AS component,
-    'Sorry' AS title,
-    'Sorry, this user name is already taken.' AS description_md,
-    'https://upload.wikimedia.org/wikipedia/commons/thumb/f/f0/Sad_face_of_a_Wayuu_Woman.jpg/640px-Sad_face_of_a_Wayuu_Woman.jpg' AS image,
-    'signup.sql' AS link,
-    'Try again' AS link_text
-WHERE NOT EXISTS (SELECT 1 FROM inserted_user);
+INSERT INTO user_info (username, password_hash)
+VALUES (:username, sqlpage.hash_password(:password))
+ON CONFLICT (username) DO NOTHING
+RETURNING 
+    'redirect' AS component,
+    'create_user_welcome_message.sql?username=' || :username AS link;
+
+-- If we are still here, it means that the user was not created
+-- because the username was already taken.
+SELECT 'redirect' AS component, 'create_user_welcome_message.sql?error&username=' || :username AS link;
