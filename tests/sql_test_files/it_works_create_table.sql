9764e02995e4e547d366723697a14a284afc77b6
lovasoa <pere.jobs@gmail.com>
2023-11-19 01:13:46 +0100

    mssql permissions fix

--- tests/sql_test_files/it_works_create_table.sql
+++ tests/sql_test_files/it_works_create_table.sql
@@ -1,7 +1,7 @@
-create table my_tmp_store(x text);
+drop table if exists my_tmp_store;
+create table my_tmp_store(x varchar(100));
 
 insert into my_tmp_store(x) values ('It works !');
 
 select 'card' as component;
 select x as description from my_tmp_store;
-drop table my_tmp_store;
