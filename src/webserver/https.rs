2d42756053d55167586b69ddf4f599ce3885935c
lovasoa <pere.jobs@gmail.com>
2023-11-22 01:44:20 +0100

    wip

---
+++ src/webserver/https.rs
@@ -0,0 +1,10 @@
+use rustls::ServerConfig;
+/* 
+pub(super) fn build_ssl_config() -> anyhow::Result<ServerConfig> {
+    let conf = ServerConfig::builder()
+        .with_safe_defaults()
+        .with_no_client_auth()
+        .with_single_cert(certs, private_key)?;
+    Ok(conf)
+}
+*/
--- src/webserver/mod.rs
+++ src/webserver/mod.rs
@@ -1,6 +1,7 @@
 pub mod database;
 pub mod error_with_status;
 pub mod http;
+pub mod https;
 
 pub use database::Database;
 pub use error_with_status::ErrorWithStatus;
