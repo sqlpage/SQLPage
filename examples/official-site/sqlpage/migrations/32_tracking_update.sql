3e854da99abd05daedfb99368259bad649368203
Olivier Auverlot <oauv@duck.com>
2024-01-07 18:07:53 +0100

    New 'center' parameter to whether to center the trakcing component.

---
+++ examples/official-site/sqlpage/migrations/32_tracking_update.sql
@@ -0,0 +1,17 @@
+INSERT INTO parameter (
+        component,
+        name,
+        description,
+        type,
+        top_level,
+        optional
+    )
+VALUES (
+        'tracking',
+        'center',
+        'Whether to center the component.',
+        'BOOLEAN',
+        TRUE,
+        TRUE
+    );
+
