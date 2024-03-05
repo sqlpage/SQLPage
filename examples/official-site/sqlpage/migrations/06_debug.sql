c0d4b8222d5a6e6b07e376622c0f90a797d3176e
lovasoa <pere.jobs@gmail.com>
2024-03-05 19:52:00 +0100

    improve the documentation for the DEBUG component
    closes #251

--- examples/official-site/sqlpage/migrations/06_debug.sql
+++ examples/official-site/sqlpage/migrations/06_debug.sql
@@ -2,7 +2,9 @@
 INSERT INTO component (name, description, icon)
 VALUES (
         'debug',
-        'Display all the parameters passed to the component. Useful for debugging: just replace the name of the component you want to debug with ''debug''.',
+        'Visualize any set of values as JSON.
+Can be used to display all the parameters passed to the component.
+Useful for debugging: just replace the name of the component you want to debug with ''debug'', and see all the top-level and row-level parameters that are passed to it, and their types.',
         'bug'
     );
 -- Insert an example usage of the http_header component into the example table
@@ -10,5 +12,14 @@ INSERT INTO example (component, description, properties)
 VALUES (
         'debug',
         'At any time, if you are confused about what data you are passing to a component, just replace the component name with ''debug'' to see all the parameters that are passed to it.',
-        JSON('[{"component": "debug"}, {"x": "y", "z": 42}, {"a": "b", "c": null}]')
+        JSON('[{"component": "debug", "my_top_level_property": true}, {"x": "y", "z": 42}, {"a": "b", "c": null}]')
+    ),
+    (
+        'debug',
+        'Show the result of a SQLPage function:
+        
+```sql
+select ''debug'' as component, sqlpage.environment_variable(''HOME'');
+```',
+        NULL
     );
