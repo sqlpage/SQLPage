b9d572ea1107f932a63fe988a3c7d55fd3bb3fae
lovasoa <pere.jobs@gmail.com>
2024-06-06 08:43:36 +0200

    update form component documentation
    see https://github.com/lovasoa/SQLpage/discussions/375#discussioncomment-9685848

--- examples/official-site/sqlpage/migrations/01_documentation.sql
+++ examples/official-site/sqlpage/migrations/01_documentation.sql
@@ -214,7 +214,7 @@ INSERT INTO parameter(component, name, description, type, top_level, optional) S
     ('reset', 'The text to display in the button at the bottom of the form that resets the form to its original state. Omit this property not to show a reset button at all.', 'TEXT', TRUE, TRUE),
     ('id', 'A unique identifier for the form, which can then be used to validate the form from a button outside of the form.', 'TEXT', TRUE, TRUE),
     -- item level
-    ('type', 'The type of input to use: text for a simple text field, textarea for a multi-line text input control, number for field that accepts only numbers, checkbox or radio for a button that is part of a group specified in the ''name'' parameter. This is set to "text" by default.', 'TEXT', FALSE, TRUE),
+    ('type', 'The type of input to use: text for a simple text field, textarea for a multi-line text input control, number to accept only numbers, checkbox or radio for a button that is part of a group specified in the ''name'' parameter, hidden for a value that will be submitted but not shown to the user. text by default.', 'TEXT', FALSE, TRUE),
     ('name', 'The name of the input field, that you can use in the target page to get the value the user entered for the field.', 'TEXT', FALSE, FALSE),
     ('label', 'A friendly name for the text field to show to the user.', 'TEXT', FALSE, TRUE),
     ('placeholder', 'A placeholder text that will be shown in the field when is is empty.', 'TEXT', FALSE, TRUE),
