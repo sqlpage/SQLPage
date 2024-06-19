d7cbcf0bca97cfb4f581167f0e2dba5dd3263dda
PChemGuy <39730837+pchemguy@users.noreply.github.com>
2024-06-19 16:22:20 +0300

    Extended menu demo

---
+++ examples/official-site/menu_demo.sql
@@ -0,0 +1,82 @@
+select 'dynamic' as component,
+'[
+    {
+        "component": "shell",
+        "title": "SQLPage",
+        "icon": "database",
+        "link": "/",
+        "menu_item": [
+            {
+                "icon": "settings",
+                "title": "",
+                "compact": true,
+                "button": true,
+                "shape": "pill",
+                "narrow": true,
+                "outline": "warning",
+                "submenu": [
+                    {
+                        "link": "/safety.sql",
+                        "icon": "user",
+                        "button": true,
+                        "shape": "pill",
+                        "size": "sm",
+                        "color": "yellow"
+                    },
+                    {
+                        "link": "/performance.sql",
+                        "icon": "logout",
+                        "button": true,
+                        "shape": "pill",
+                        "size": "sm",
+                        "outline": "warning"
+
+                    }
+                ]
+            },
+            {
+                "icon": "database",
+                "title": "Dummy",
+                "button": true,
+                "shape": "pill",
+                "narrow": true,
+                "color": "green"
+            },
+            {
+                "title": "Examples",
+                "icon": "trash",
+                "submenu": [
+                    {
+                        "link": "/examples/tabs.sql",
+                        "icon": "device-floppy",
+                        "title": "Tabs"
+                    },
+                    {
+                        "link": "/examples/layouts.sql",
+                        "title": "Layouts",
+                        "button": true,
+                        "color": "primary"
+                    },
+                    {
+                        "link": "/examples/multistep-form",
+                        "title": "Forms"
+                    }
+                ]
+            },
+            {
+                "title": "Community",
+                "compact": true,
+                "submenu": [
+                    {
+                        "link": "blog.sql",
+                        "title": "Blog"
+                    },
+                    {
+                        "link": "//github.com/lovasoa/sqlpage/issues",
+                        "title": "Report a bug"
+                    }
+                ]
+            }
+        ]
+    }
+]' AS properties;
