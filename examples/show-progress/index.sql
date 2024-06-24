6761c4de35e41f82f2d98ca72be4d91a91d073cf
Daniel Sheffield <Daniel.Sheffield@first-watch.co.nz>
2024-06-24 12:18:18 +1200

    add example of progress on it's own

--- examples/show-progress/index.sql
+++ examples/show-progress/index.sql
@@ -1,32 +1,49 @@
-SELECT 'spinner-start' AS component,
+SELECT 'shell' AS component,
+  'dark' AS theme;
+
+SELECT 'loader-start' AS component,
   -- default is "spinner-border"
   "spinner-grow text-red" AS spinner;
-SELECT 'spinner-progress' AS component,
+
+SELECT 'progress' AS component,
+  'sm' AS size,
+  'yellow' AS color,
   '0' AS percent,
   'Sleeping 1 second' AS stage;
 SELECT sqlpage.exec('sleep', '1');
 
-SELECT 'spinner-progress' AS component,
-  '20' AS percent,
-  'Sleeping 1 more second' AS stage;
+/* percent property is optional */
+SELECT 'progress' AS component,
+  NULL AS percent,
+  'sm' AS size,
+  'yellow' AS color,
+  'Doing something' AS stage;
 SELECT sqlpage.exec('sleep', '1');
 
 /* stage property is optional */
-SELECT 'spinner-progress' AS component,
+SELECT 'progress' AS component,
+  'sm' AS size,
+  'yellow' AS color,
   '40' AS percent;
 SELECT sqlpage.exec('sleep', '1');
 
-SELECT 'spinner-progress' AS component,
-  '60' AS percent,
-  'Sleeping 2 seconds' AS stage;
-SELECT sqlpage.exec('sleep', '2');
+/* multiple rows */
+SELECT 'progress' AS component, 'sm' AS size, 'yellow' AS color;
+SELECT '70' AS percent, 'Sleeping 1 second' AS stage
+SELECT sqlpage.exec('sleep', '1');
+SELECT '80' AS percent, 'Sleeping 1 more second' AS stage;
+SELECT sqlpage.exec('sleep', '1');
+SELECT '90' AS percent, 'Sleeping 1 second again' AS stage;
+SELECT sqlpage.exec('sleep', '1');
 
-SELECT 'spinner-progress' AS component,
-  '100' AS percent;
+SELECT '100' AS percent;
 
-SELECT 'spinner-stop' AS component;
+SELECT 'loader-stop' AS component;
 
 SELECT 'text' AS component,
   'It works!' AS title,
   TRUE AS center,
   'Page is loaded.' AS contents;
+
+-- can use progress on it's own
+SELECT 'progress' AS component, 'Waiting for user' AS stage;
