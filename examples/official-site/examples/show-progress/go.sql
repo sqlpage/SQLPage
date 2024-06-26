df752bfdd62f832a20e2b11b1595df64ae4e4cf8
Daniel Sheffield <Daniel.Sheffield@first-watch.co.nz>
2024-06-26 17:21:54 +1200

    improve top-level params of spinner component and add documentation

--- examples/official-site/examples/show-progress/go.sql
+++ examples/official-site/examples/show-progress/go.sql
@@ -1,14 +1,14 @@
 SELECT 'shell' AS component, 'light' AS theme;
 
 -- can disable the spinner to show only progress bar
-SELECT 'loader-start' AS component,  NULL AS spinner;
+SELECT 'loader-start' AS component,  '' AS spinner;
 
 SELECT 'progress' AS component,
   NULL AS percent,
   'sm' AS size,
   'yellow' AS color,
   'Working on it' AS stage;
-SELECT sqlpage.exec('sleep', '3');
+SELECT sqlpage.fetch('https://example.com');
 
 SELECT 'loader-stop' AS component;
 
--- examples/official-site/examples/show-progress/index.sql
+++ examples/official-site/examples/show-progress/index.sql
@@ -2,14 +2,16 @@ SELECT 'shell' AS component, 'dark' AS theme;
 
 SELECT 'loader-start' AS component,
   -- pick from the tabler spinners: https://tabler.io/docs/components/spinners
-  "spinner-border spinner-border-sm text-red" AS spinner;
+  "spinner-border" spinner,
+  "sm" AS size,
+  "red" AS color;
 
 SELECT 'progress' AS component,
   'sm' AS size,
   'yellow' AS color,
   '0' AS percent,
-  'Sleeping 1 second' AS stage;
-SELECT sqlpage.exec('sleep', '1');
+  'Fething data' AS stage;
+SELECT sqlpage.fetch('https://example.com');
 
 /* percent property is optional */
 SELECT 'progress' AS component,
@@ -17,23 +19,23 @@ SELECT 'progress' AS component,
   'sm' AS size,
   'yellow' AS color,
   'Doing something' AS stage;
-SELECT sqlpage.exec('sleep', '1');
+SELECT sqlpage.fetch('https://example.com');
 
 /* stage property is optional */
 SELECT 'progress' AS component,
   'sm' AS size,
   'yellow' AS color,
   '40' AS percent;
-SELECT sqlpage.exec('sleep', '1');
+SELECT sqlpage.fetch('https://example.com');
 
 /* multiple rows */
 SELECT 'progress' AS component, 'sm' AS size, 'yellow' AS color;
-SELECT '70' AS percent, 'Sleeping 1 second' AS stage
-SELECT sqlpage.exec('sleep', '1');
-SELECT '80' AS percent, 'Sleeping 1 more second' AS stage;
-SELECT sqlpage.exec('sleep', '1');
-SELECT '90' AS percent, 'Sleeping 1 second again' AS stage;
-SELECT sqlpage.exec('sleep', '1');
+SELECT '70' AS percent, 'Fetching data' AS stage
+SELECT sqlpage.fetch('https://example.com');
+SELECT '80' AS percent, 'Fetching more data' AS stage;
+SELECT sqlpage.fetch('https://example.com');
+SELECT '90' AS percent, 'Fetching again' AS stage;
+SELECT sqlpage.fetch('https://example.com');
 
 SELECT '100' AS percent;
 
@@ -45,7 +47,7 @@ SELECT 'text' AS component,
   'Page is loaded.' AS contents;
 
 SELECT 'button' AS component;
-SELECT 'Go' AS title, '/go.sql' AS link;
+SELECT 'Go' AS title, './go.sql' AS link;
 
 -- can use progress on it's own
 SELECT 'progress' AS component, 'sm' AS size, 'Waiting for user' AS stage;
