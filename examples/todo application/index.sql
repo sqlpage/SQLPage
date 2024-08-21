920c58a5df7a8adb3d9332eec9327e8285615024
Saturos <ajgp10@gmail.com>
2024-08-21 15:21:47 +0200

    ... Finally fixed the parenthesis problem on the docs

--- examples/todo application/index.sql
+++ examples/todo application/index.sql
@@ -18,3 +18,10 @@ select
     'green' as color,
     'Add new todo'  as title,
     'circle-plus'  as icon;
+select
+    'card' as component,
+    'Accordion component showcase' as title;
+
+select
+    '/accordion.sql?_sqlpage_embed' as embed,
+    12 as width;
--- examples/todo application/shell.sql
+++ examples/todo application/shell.sql
@@ -1,4 +1,7 @@
 select 'shell' as component,
     printf('Todo list (%d)', count(*)) as title,
-    'timeline' as menu_item
+    'timeline' as menu_item,
+    TRUE as sidebar,
+    'dark' as theme,
+    './sqlpage.css' as css
 from todos;
