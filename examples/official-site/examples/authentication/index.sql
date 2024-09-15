dd3d9a203844a2f81c5c207d0e2b02b0dec040c0
lovasoa <contact@ophir.dev>
2024-09-15 11:50:12 +0200

    role is a reserved keyword in sql

--- examples/official-site/examples/authentication/index.sql
+++ examples/official-site/examples/authentication/index.sql
@@ -1,13 +1,13 @@
 -- redirect the user to the login page if they are not logged in
 -- this query should be present at the top of every page that requires authentication
-set role = (select role from users natural join user_sessions where session_token = sqlpage.cookie('session_token'));
-select 'redirect' as component, 'login.sql' as link where $role is null;
+set user_role = (select role from users natural join user_sessions where session_token = sqlpage.cookie('session_token'));
+select 'redirect' as component, 'login.sql' as link where $user_role is null;
 
 select 'dynamic' as component, 
     json_insert(properties, '$[0].menu_item[#]', 'logout') as properties
 FROM example WHERE component = 'shell' LIMIT 1;
 
-select 'alert' as component, 'info' as color, CONCAT('You are logged in as ', $role) as title;
+select 'alert' as component, 'info' as color, CONCAT('You are logged in as ', $user_role) as title;
 
 select 'text' as component, '
 # Authentication
