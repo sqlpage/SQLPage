fbf994892dc1964e2be31ab2b0c1492847e8db60
lovasoa <contact@ophir.dev>
2024-10-05 23:51:12 +0200

    json_object is different on postgres

--- tests/sql_test_files/it_works_columns_component_json_nomssql_nopostgres.sql
+++ tests/sql_test_files/it_works_columns_component_json_nomssql_nopostgres.sql
@@ -1,4 +1,4 @@
--- this test is skipped on MSSQL because its json_object function has a different syntax
+-- this test is skipped on MSSQL and postgres because their json_object function has a different syntax
 select 'columns' as component;
 select
 	JSON_OBJECT('description', 'It works !') as item,
