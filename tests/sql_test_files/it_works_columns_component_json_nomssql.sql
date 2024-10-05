4cafa1eac0574dc7354c56ccc32100fcd783494f
lovasoa <contact@ophir.dev>
2024-10-05 23:20:29 +0200

    add a machanism to skip some tests on some dbs

--- tests/sql_test_files/it_works_columns_component_json_nomssql.sql
+++ tests/sql_test_files/it_works_columns_component_json_nomssql.sql
@@ -1,5 +1,5 @@
+-- this test is skipped on MSSQL because its json_object function has a different syntax
 select 'columns' as component;
-
 select
 	JSON_OBJECT('description', 'It works !') as item,
 	JSON_OBJECT('description', 'It works !') as item
