81c904166fefecccef519bdb98d754ab4df49be6
lovasoa <contact@ophir.dev>
2024-10-15 13:19:37 +0200

    pgconf 2024

---
+++ examples/official-site/sqlpage/migrations/53_blog_pgconf2024.sql
@@ -0,0 +1,28 @@
+
+INSERT INTO blog_posts (title, description, icon, created_at, content)
+VALUES
+    (
+        'Come and see us in Athens !',
+        'PGConf 2024 is coming to Athens, Greece. And we will be there to talk about SQLPage & Archeology !',
+        'microphone-2',
+        '2024-10-15',
+        '
+# PostgreSQL & SQLPage at PGConf 2024
+
+We have been invited to give a talk at PGConf 2024 in Athens, Greece.
+
+Last year, we gave a general introduction to SQLPage at PGConf.eu 2023 in Prague.
+
+This year, we will focus on a more specific use case:
+using SQLPage to build a power-tool for archaeologists to explore and understand archaeological sites,
+radically changing the way archaeologists work, and allowing them to
+drastically reduce the time spent on data entry and management.
+
+There will be two presenters:
+ - **Ophir Lojkine**, the creator and maintainer of SQLPage, will present the project and its capabilities.
+ - **Thomas Guillemard**, an archaeologist from France''s National Institute for Preventive Archaeological Research, will present the project''s use case and its benefits.
+
+Come and see us in Athens !
+
+https://www.postgresql.eu/events/pgconfeu2024/schedule/session/5707-unearthing-the-past-with-postgresql-how-open-source-is-revolutionizing-digital-archaeology/
+');
