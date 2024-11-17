a104c7d3c9efb9d8859c7d6ee75596c917bf2512
Ophir LOJKINE <contact@ophir.dev>
2024-11-17 10:43:44 +0100

    Update examples/official-site/sqlpage/migrations/01_documentation.sql

--- examples/official-site/sqlpage/migrations/01_documentation.sql
+++ examples/official-site/sqlpage/migrations/01_documentation.sql
@@ -251,7 +251,7 @@ INSERT INTO parameter(component, name, description, type, top_level, optional) S
     ('max', 'The maximum value to accept for an input of type number', 'REAL', FALSE, TRUE),
     ('checked', 'Used only for checkboxes and radio buttons. Indicates whether the checkbox should appear as already checked.', 'BOOLEAN', FALSE, TRUE),
     ('multiple', 'Used only for select elements. Indicates that multiple elements can be selected simultaneously. When using multiple, you should add square brackets after the variable name: ''my_variable[]'' as name', 'BOOLEAN', FALSE, TRUE),
-    ('emptyoption', 'Used only for select elements. Add empty option at first', 'BOOLEAN', FALSE, TRUE),
+    ('empty_option', 'Only for inputs of type `select`. Adds an empty option before the ones defined in `options`.', 'BOOLEAN', FALSE, TRUE),
     ('searchable', 'For select and multiple-select elements, displays them with a nice dropdown that allows searching for options.', 'BOOLEAN', FALSE, TRUE),
     ('dropdown', 'An alias for "searchable".', 'BOOLEAN', FALSE, TRUE),
     ('create_new', 'In a multiselect with a dropdown, this option allows the user to enter new values, that are not in the list of options.', 'BOOLEAN', FALSE, TRUE),
