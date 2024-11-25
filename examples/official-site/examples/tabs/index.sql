51b0f2371f027b8ea984542ac05316791ba35e89
lovasoa <contact@ophir.dev>
2024-11-25 01:17:07 +0100

    updated examples

--- examples/official-site/examples/tabs/index.sql
+++ examples/official-site/examples/tabs/index.sql
@@ -9,12 +9,13 @@ select 'Image Gallery', 'image gallery with user uploads', 'sqlite', 'Create an 
 select 'Developer UI', 'SQLPage developer user interface', 'postgres', 'A web-based interface for managing SQLPage files and database tables.' union all
 select 'Corporate Game', 'corporate-conundrum', 'sqlite', 'An interactive multiplayer board game with real-time updates.' union all
 select 'Roundest Pokemon', 'roundest_pokemon_rating', 'sqlite', 'Demo app with a distinct non-default design, using custom HTML templates for everything.' union all
-select 'Todo Application', 'todo application (PostgreSQL)', 'sqlite', 'A full-featured todo list application with PostgreSQL backend.' union all
+select 'Todo Application', 'todo application (PostgreSQL)', 'postgres', 'A full-featured todo list application with PostgreSQL backend.' union all
 select 'MySQL & JSON', 'mysql json handling', 'mysql', 'Learn advanced JSON manipulation in MySQL to build advanced SQLPage applications.' union all
 select 'Simple Website', 'simple-website-example', 'sqlite', 'Basic website example with navigation and data management.' union all
 select 'Geographic App', 'PostGIS - using sqlpage with geographic data', 'postgres', 'Use SQLPage to create and manage geodata.' union all
 select 'Multi-step form', 'forms-with-multiple-steps', 'sqlite', 'Guide to the implementation of forms that spread over multiple pages.' union all
 select 'Custom HTML & JS', 'custom form component', 'mysql', 'Building a custom form component with a dynamic widget using HTML and javascript.' union all
+select 'Splitwise Clone', 'splitwise', 'sqlite', 'An expense tracker app to split expenses with your friends, with nice debt charts.' union all
 select 'Advanced Forms with MS SQL Server', 'microsoft sql server advanced forms', 'sql server', 'Forms with multi-value dropdowns, using SQL Server and its JSON functions.';
 
 select 'tab' as component, true as center;
