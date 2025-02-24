e75fc22d7f3287cacd16fcfeef8f5959e81b91f2
lovasoa <contact@ophir.dev>
2025-02-24 22:22:03 +0100

    fix tests on mssql
    no json_extract

--- tests/sql_test_files/it_works_fetch_with_meta_error.sql
+++ tests/sql_test_files/it_works_fetch_with_meta_error.sql
@@ -2,6 +2,6 @@ set res = sqlpage.fetch_with_meta('http://not-a-real-url');
 
 select 'text' as component,
     case
-        when json_extract($res, '$.error') LIKE '%Request failed%' then 'It works !'
+        when $res LIKE '%"error":"Request failed%' then 'It works !'
         else CONCAT('Error! Got: ', $res)
     end as contents; 
