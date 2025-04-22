407bc81664c7ca47fb51fcafc46015eeb36c2dbf
lovasoa <contact@ophir.dev>
2025-04-22 11:05:37 +0200

    improve the documentation for the redirect component

--- examples/official-site/sqlpage/migrations/09_redirect.sql
+++ examples/official-site/sqlpage/migrations/09_redirect.sql
@@ -2,13 +2,38 @@ INSERT INTO component (name, description, icon, introduced_in_version)
 VALUES (
         'redirect',
         'Redirects the user to another page.
-        This component is useful for implementing redirects after a form submission,
-        or to redirect users to a login page if they are not logged in.
-        
-        Contrary to the http_header component, this component completely stops the execution of the page after it is called,
-        so it is suitable to use to hide sensitive information from users that are not logged in, for example.
 
-        Since it uses an HTTP header to redirect the user, it is not possible to use this component after the page has started being sent to the browser.',
+This component helps you:
+1. Send users to a different page
+1. Stop execution of the current page
+
+### Conditional logic
+
+There is no `IF` statement in SQL. Even when you use a [`CASE` expression](https://modern-sql.com/caniuse/case_(simple)), all branches are always evaluated (and only one is returned).
+
+To conditionally execute a component or a [SQLPage function](/functions.sql), you can use the `redirect` component.
+A common use case is error handling. You may want to process with the rest of a page only when certain pre-conditions are met.
+
+```sql
+SELECT
+    ''redirect'' AS component,
+    ''error_page.sql'' AS link
+WHERE NOT your_condition;
+
+-- The rest of the page is only executed if the condition is true
+```
+### Technical limitation
+
+You must use this component **at the beginning of your SQL file**, before any other components that might send content to the browser.
+Since the component needs to tell the browser to go to a different page by sending an *HTTP header*,
+it will fail if the HTTP headers have already been sent by the time it is executed.
+
+> **Important difference from [http_header](?component=http_header)**
+>
+> This component completely stops the page from running after it''s called.
+> This makes it a good choice for protecting sensitive information from unauthorized users.
+
+',
         'arrow-right',
         '0.7.2'
     );
