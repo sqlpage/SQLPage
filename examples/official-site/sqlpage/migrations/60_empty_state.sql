689c614c0728520d717b5e31dd3925750867facb
Olivier Auverlot <olivier.auverlot@gmail.com>
2025-04-23 18:44:46 +0200

    Correction and new example

--- examples/official-site/sqlpage/migrations/60_empty_state.sql
+++ examples/official-site/sqlpage/migrations/60_empty_state.sql
@@ -6,9 +6,9 @@ INSERT INTO parameter(component, name, description, type, top_level, optional) S
     ('status_code','HTTP status code displayed on the top of the empty state.','INTEGER',TRUE,TRUE),
     ('icon','Name of an icon to be displayed on the top of the empty state.','ICON',TRUE,TRUE),
     ('image','The URL (absolute or relative) of an image to display at the top of the empty state.','URL',TRUE,TRUE),
-    ('information','A short text displayed below the title.','TEXT',TRUE,FALSE),
-    ('btn_title','The text displayed on the button.','TEXT',TRUE,FALSE),
-    ('btn_icon','Name of an icon to be displayed on the left side of the button.','ICON',TRUE,FALSE),
+    ('description','A short text displayed below the title.','TEXT',TRUE,TRUE),
+    ('link_text','The text displayed on the button.','TEXT',TRUE,FALSE),
+    ('link_icon','Name of an icon to be displayed on the left side of the button.','ICON',TRUE,FALSE),
     ('link','The URL to which the button should navigate when clicked.','URL',TRUE,FALSE)
 ) x;
 
@@ -20,9 +20,21 @@ The empty_state component provides users with informative and visually appealing
         "component": "empty_state",
         "title": "No results found",
         "status_code": 404,
-        "information": "Try adjusting your search or filter to find what you''re looking for.",
-        "btn_title": "Search again",
-        "btn_icon": "search",
+        "description": "Try adjusting your search or filter to find what you''re looking for.",
+        "link_text": "Search again",
+        "link_icon": "search",
+        "link": "#"
+    }]')),
+    ('empty_state', '
+It''s possible to use an icon or an image to illustrate the problem.
+',
+    json('[{
+        "component": "empty_state",
+        "title": "A critical problem has occurred",
+        "icon": "mood-wrrr",
+        "description": "Thank you for restarting your browser and crossing your fingers.",
+        "link_text": "Close and restart",
+        "link_icon": "rotate-clockwise",
         "link": "#"
     }]'));
 
