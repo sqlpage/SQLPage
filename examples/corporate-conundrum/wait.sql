e0fac05dc599cfd0df43965232995a0ff04264ca
ppom <>
2025-04-29 12:00:00 +0200

    Fix SQLite examples having PostgreSQL :: syntax
    According to discussion here
    https://github.com/sqlpage/SQLPage/pull/897#issuecomment-2835516830

--- examples/corporate-conundrum/wait.sql
+++ examples/corporate-conundrum/wait.sql
@@ -1,8 +1,8 @@
 -- Redirect to the next question when all players have answered
 set page_params = json_object('game_id', $game_id, 'player', $player);
 select CASE
-        (SELECT count(*) FROM answers WHERE question_id = $question_id AND game_id = $game_id::integer)
-        WHEN (SELECT count(*) FROM players WHERE game_id = $game_id::integer)
+        (SELECT count(*) FROM answers WHERE question_id = $question_id AND game_id = CAST($game_id AS INTEGER))
+        WHEN (SELECT count(*) FROM players WHERE game_id = CAST($game_id AS INTEGER))
         THEN '0; ' || sqlpage.link('next-question.sql', $page_params)
         ELSE 3
     END as refresh,
@@ -11,10 +11,10 @@ FROM sqlpage_shell;
 
 -- Insert the answer into the answers table
 INSERT INTO answers(game_id, player_name, question_id, answer_value)
-SELECT $game_id::integer as game_id,
+SELECT CAST($game_id AS INTEGER) as game_id,
     $player as player_name,
-    $question_id::integer as question_id,
-    $answer::integer as answer_value
+    CAST($question_id AS INTEGER) as question_id,
+    CAST($answer AS INTEGER) as answer_value
 WHERE $answer IS NOT NULL;
 -- Redirect to the next question
 SELECT 'text' as component,
@@ -22,11 +22,11 @@ SELECT 'text' as component,
 select group_concat(name, ', ') as contents,
     TRUE as bold
 from players
-where game_id = $game_id::integer
+where game_id = CAST($game_id AS INTEGER)
     and not EXISTS (
         SELECT 1
         FROM answers
-        WHERE answers.game_id = $game_id::integer
+        WHERE answers.game_id = CAST($game_id AS INTEGER)
             AND answers.player_name = players.name
-            AND answers.question_id = $question_id::integer
+            AND answers.question_id = CAST($question_id AS INTEGER)
     );
