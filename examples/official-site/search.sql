845e0371ec49b85889bb16be010b5cd7acf56a7d
lovasoa <contact@ophir.dev>
2025-05-08 01:01:46 +0200

    sql-page.com search: dynamic component retrieval and improve link data handling

--- examples/official-site/search.sql
+++ examples/official-site/search.sql
@@ -6,7 +6,10 @@ END
 SELECT 'redirect' as component, $redirect as link WHERE $redirect IS NOT NULL;
 
 
-select 'dynamic' as component, properties FROM example WHERE component = 'shell' LIMIT 1;
+select 'dynamic' as component, json_patch(json_extract(properties, '$[0]'), json_object(
+    'title', coalesce($search || ' | ', '') || 'SQLPage documentation search'
+)) as properties
+FROM example WHERE component = 'shell' LIMIT 1;
 
 SELECT 'form' as component,
     'GET' as method,
@@ -37,34 +40,57 @@ WHERE $search IS NOT NULL;
 
 WITH search_results AS (
     SELECT 
+        COALESCE(
+              component_name || ' component: parameter ' || parameter_name
+            , component_name || ' component' || IF(component_example_description IS NULL, '', ' example')
+            , 'blog: ' || blog_title
+            , 'sqlpage.' || function_name || '(...' || function_parameter_name || '...)'
+            , 'sqlpage.' || function_name || '(...)'
+        ) as title,
+        COALESCE(
+            component_description,
+            parameter_description,
+            blog_description,
+            function_parameter_description,
+            function_description,
+            component_example_description
+        ) as description,
         CASE 
-            WHEN parameter_name IS NOT NULL THEN component_name || ' component: parameter ' || parameter_name
-            WHEN component_name IS NOT NULL THEN component_name || ' component'
-            WHEN blog_title IS NOT NULL THEN 'blog: ' || blog_title
-            WHEN function_parameter_name IS NOT NULL THEN function_name || '(...' || function_parameter_name || '...)'
-            WHEN function_name IS NOT NULL THEN function_name || '(...)'
-        END as title,
-        CASE 
-            WHEN component_description IS NOT NULL THEN component_description
-            WHEN parameter_description IS NOT NULL THEN parameter_description
-            WHEN blog_description IS NOT NULL THEN blog_description
-            WHEN function_description IS NOT NULL THEN function_description
-            WHEN function_parameter_description IS NOT NULL THEN function_parameter_description
-        END as description,
-        CASE 
-            WHEN component_name IS NOT NULL THEN json_object('page', '/component.sql', 'parameters', json_object('component', component_name))
-            WHEN parameter_name IS NOT NULL THEN json_object('page', '/component.sql', 'parameters', json_object('component', (
-                SELECT component FROM parameter 
-                WHERE name = parameter_name 
-                LIMIT 1
-            )))
-            WHEN blog_title IS NOT NULL THEN json_object('page', '/blog.sql', 'parameters', json_object('post', blog_title))
-            WHEN function_name IS NOT NULL THEN json_object('page', '/functions.sql', 'parameters', json_object('function', function_name))
-            WHEN function_parameter_name IS NOT NULL THEN json_object('page', '/functions.sql', 'parameters', json_object('function', (
-                SELECT function FROM sqlpage_function_parameters 
-                WHERE name = function_parameter_name 
-                LIMIT 1
-            )))
+            WHEN component_name IS NOT NULL THEN 
+                json_object(
+                    'page', '/component.sql',
+                    'parameters', json_object('component', component_name)
+                )
+            WHEN parameter_name IS NOT NULL THEN 
+                json_object(
+                    'page', '/component.sql',
+                    'parameters', json_object('component', (
+                        SELECT component 
+                        FROM parameter 
+                        WHERE name = parameter_name 
+                        LIMIT 1
+                    ))
+                )
+            WHEN blog_title IS NOT NULL THEN 
+                json_object(
+                    'page', '/blog.sql',
+                    'parameters', json_object('post', blog_title)
+                )
+            WHEN function_name IS NOT NULL THEN 
+                json_object(
+                    'page', '/functions.sql',
+                    'parameters', json_object('function', function_name)
+                )
+            WHEN function_parameter_name IS NOT NULL THEN 
+                json_object(
+                    'page', '/functions.sql',
+                    'parameters', json_object('function', (
+                        SELECT function 
+                        FROM sqlpage_function_parameters 
+                        WHERE name = function_parameter_name 
+                        LIMIT 1
+                    ))
+                )
         END as link_data,
         rank
     FROM documentation_fts
@@ -72,10 +98,11 @@ WITH search_results AS (
     AND documentation_fts = $search
 )
 SELECT 
-    title,
-    description,
+    max(title) as title,
+    max(description) as description,
     sqlpage.link(link_data->>'page', link_data->'parameters') as link
 FROM search_results
+GROUP BY link_data
 ORDER BY 
     rank,
     CASE 
