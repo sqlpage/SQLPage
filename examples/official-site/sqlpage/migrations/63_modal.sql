e06f9a57d04fecc9ead7b05948b71098733aeccd
Olivier Auverlot <olivier.auverlot@gmail.com>
2025-06-09 07:48:04 +0200

    Update examples/official-site/sqlpage/migrations/63_modal.sql

--- examples/official-site/sqlpage/migrations/63_modal.sql
+++ examples/official-site/sqlpage/migrations/63_modal.sql
@@ -7,7 +7,7 @@ INSERT INTO parameter(component, name, description, type, top_level, optional) S
     ('contents','A paragraph of text to display, without any formatting, without having to make additional queries.','TEXT',FALSE,TRUE),
     ('contents_md','Rich text in the markdown format. Among others, this allows you to write bold text using **bold**, italics using *italics*, and links using [text](https://example.com).','TEXT',FALSE,TRUE),
     ('unsafe_contents_md','Markdown format with html blocks. Use this only with trusted content. See the html-blocks section of the Commonmark spec for additional info.','TEXT',FALSE,TRUE),
-    ('scrollable','Dreate a scrollable modal that allows scroll the modal body.','BOOLEAN',TRUE,TRUE),
+    ('scrollable','Create a scrollable modal that allows scroll the modal body.','BOOLEAN',TRUE,TRUE),
     ('class','Class attribute added to the container in HTML. It can be used to apply custom styling to this item through css.','TEXT',TRUE,TRUE),
     ('id','ID attribute added to the container in HTML. It can be used to target this item through css or for displaying this item.','TEXT',TRUE,FALSE)
 ) x;
