37fb4beb156a907dc79128a0bf6e4e77abb9c821
Francesco Cattoglio <francesco.cattoglio@gmail.com>
2025-06-20 15:09:23 +0000

    Added a test, improved the comments in cargo config file

---
+++ tests/sql_test_files/it_works_sqrt.sql
@@ -0,0 +1,7 @@
+set number_three = sqrt(9.0);
+
+select 'text' as component, 
+    case $number_three
+        when '3.0' then 'It works !'
+        else 'error: ' || coalesce($number_three, 'NULL')
+    end AS contents;
