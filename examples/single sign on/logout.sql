627b4789cbc1297f2f9f53c651e965ab24fd77bc
lovasoa <contact@ophir.dev>
2025-07-30 17:25:12 +0200

    improve sso example (logout + auth custom)

--- examples/single sign on/logout.sql
+++ examples/single sign on/logout.sql
@@ -6,4 +6,7 @@ select
 
 select
     'redirect' as component,
-    'http://localhost:8181/realms/sqlpage_demo/protocol/openid-connect/logout' as link;
+    sqlpage.link('http://localhost:8181/realms/sqlpage_demo/protocol/openid-connect/logout', json_object(
+    'post_logout_redirect_uri', 'http://localhost:8080/',
+    'id_token_hint', sqlpage.cookie('sqlpage_auth')
+    )) as link;
