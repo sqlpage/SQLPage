f71c7ac864b5d66cd7dc68f2d63482f1be10a159
lovasoa <contact@ophir.dev>
2025-10-09 17:54:22 +0200

    add missing arm64 dep

--- scripts/setup-cross-compilation.sh
+++ scripts/setup-cross-compilation.sh
@@ -18,7 +18,7 @@ elif [ "$TARGETARCH" = "arm64" ]; then
     echo "aarch64-unknown-linux-gnu" > /tmp/TARGET
     echo "aarch64-linux-gnu-gcc" > /tmp/LINKER
 
-    apt-get install -y gcc-aarch64-linux-gnu libgcc-s1-arm64-cross
+    apt-get install -y gcc-aarch64-linux-gnu libgcc-s1-arm64-cross make
 
     LIBDIR="/usr/aarch64-linux-gnu/lib"
 elif [ "$TARGETARCH" = "arm" ]; then
