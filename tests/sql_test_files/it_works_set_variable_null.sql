a59a391446999dee42c168ab75dac2af1648839e
lovasoa <contact@ophir.dev>
2025-11-24 11:28:52 +0100

    ensure set_variable only takes into account GET variables, not SET

--- tests/sql_test_files/it_works_set_variable_null.sql
+++ tests/sql_test_files/it_works_set_variable_null.sql
@@ -1,5 +1,5 @@
-set url = sqlpage.set_variable('x', null);
 set path = sqlpage.path();
+set url = sqlpage.set_variable('x', null);
 select 'text' as component,
     case
         when $url = $path THEN 'It works !'
