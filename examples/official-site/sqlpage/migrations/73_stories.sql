83df6c07cb66ad5d10c87a958f78cfce79f0eb7f
Olivier Auverlot <olivier.auverlot@icloud.com>
2026-01-25 07:47:28 +0100

    Integration of the sucess stories page

---
+++ examples/official-site/sqlpage/migrations/73_stories.sql
@@ -0,0 +1,50 @@
+CREATE TABLE stories (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    title TEXT NOT NULL,
+    publication_date TEXT NOT NULL,
+    contents_md TEXT NOT NULL,
+    optional_contents_md TEXT,
+    image TEXT,
+    website TEXT,
+    git_repository TEXT,
+    tags JSON
+);
+
+INSERT INTO stories(title, publication_date, contents_md, optional_contents_md, image, website, git_repository, tags) VALUES
+    (
+        'User Management',
+        '2025-11-20 21:15:00',
+        'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+        'Lorem ipsum dolor sit amet consectetur adipiscing elit. Quisque faucibus ex sapien vitae pellentesque sem placerat. In id cursus mi pretium tellus duis convallis. Tempus leo eu aenean sed diam urna tempor. Pulvinar vivamus fringilla lacus nec metus bibendum egestas. Iaculis massa nisl malesuada lacinia integer nunc posuere. Ut hendrerit semper vel class aptent taciti sociosqu. Ad litora torquent per conubia nostra inceptos himenaeos.',
+        NULL,
+        NULl,
+        NULL,
+        '[]'
+    ),(
+        'API REST',
+        '2025-11-22 20:35:00',
+        'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+        NULL,
+        NULL,
+        'https://github.com/api',
+        NULL,
+        '["PostgreSQL"]'
+    ),(
+        'PowerDashboard',
+        '2025-11-25 16:15:00',
+        'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+        'Lorem ipsum dolor sit amet consectetur adipiscing elit. Quisque faucibus ex sapien vitae pellentesque sem placerat. In id cursus mi pretium tellus duis convallis. Tempus leo eu aenean sed diam urna tempor. Pulvinar vivamus fringilla lacus nec metus bibendum egestas. Iaculis massa nisl malesuada lacinia integer nunc posuere. Ut hendrerit semper vel class aptent taciti sociosqu. Ad litora torquent per conubia nostra inceptos himenaeos.',
+        'dashboard.jpg',
+        'https://www.powerdashboard.com',
+        'https://github.com/powerdashboard',
+        '["postgreSQL","MIT"]'
+    ),(
+        'SmallWorld',
+        '2025-11-25 12:15:00',
+        'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+        'Lorem ipsum dolor sit amet consectetur adipiscing elit. Quisque faucibus ex sapien vitae pellentesque sem placerat. In id cursus mi pretium tellus duis convallis. Tempus leo eu aenean sed diam urna tempor. Pulvinar vivamus fringilla lacus nec metus bibendum egestas. Iaculis massa nisl malesuada lacinia integer nunc posuere. Ut hendrerit semper vel class aptent taciti sociosqu. Ad litora torquent per conubia nostra inceptos himenaeos.',
+        'ulille.jpg',
+        NULL,
+        'https://github.com/smallworld',
+        '["MariaDB"]'
+    );
---
+++ examples/official-site/stories.sql
@@ -0,0 +1,50 @@
+SELECT 'http_header' AS component, 
+    'no-store, no-cache, must-revalidate, max-age=0' AS "Cache-Control",
+    'no-cache' AS "pragma",
+    '0' AS "expires",
+    '<https://sql-page.com/safety>; rel="canonical"' AS "Link";
+
+SELECT 'dynamic' AS component, json_patch(json_extract(properties, '$[0]'), json_object(
+    'title', 'SQLPage Success Stories'
+)) AS properties
+FROM example WHERE component = 'shell' LIMIT 1;
+
+SET MAX_WIDTH = 300;
+
+SELECT 
+    'alert'                    AS component,
+    CONCAT('You have selected the "', $filter, '" filter.') AS title,
+    'filter'                   AS icon,
+    'teal'                     AS color,
+    TRUE                       AS dismissible,
+    '[Click here to deactivate it.](stories)' AS description_md
+WHERE $filter IS NOT NULL
+
+SELECT
+    'stories' AS component, 
+    id,
+    title,
+    publication_date,
+    tags,
+    CASE
+        WHEN LENGTH(contents_md) > CAST($MAX_WIDTH AS INTEGER) THEN SUBSTR(contents_md, 1, CAST($MAX_WIDTH AS INTEGER)) || '...'
+        ELSE contents_md
+    END as truncated_contents
+FROM stories
+WHERE 
+        $id IS NULL
+  AND   ($filter IS NULL OR EXISTS (SELECT 1 FROM json_each(tags) WHERE value LIKE $filter COLLATE NOCASE))
+ORDER BY publication_date DESC;
+
+SELECT
+    'story' AS component, 
+    id,
+    title,
+    publication_date,
+    contents_md,
+    optional_contents_md,
+    image,
+    website,
+    git_repository
+FROM stories
+WHERE id = $id;
