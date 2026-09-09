443c6580340d9fe10d44b4d32b72000a899e42b9
Ophir Lojkine <contact@ophir.dev>
2026-09-09 09:00:59 +0000

    test(chart): remove unused serialization fixture

--- tests/components/chart_point_serialization.sql
+++
@@ -1,7 +0,0 @@
-SELECT 'chart' AS component, 'It works !' AS title;
-SELECT 'plain' AS x, '1' AS y;
-SELECT 'colored' AS x, '2' AS y, 'red' AS color;
-SELECT 'sized' AS x, '3' AS y, '30' AS z;
-SELECT 'both' AS x, '4' AS y, 'green' AS color, '40' AS z;
-SELECT 'linked' AS x, '5' AS y, '/edit.sql?id=5' AS link;
-SELECT '70' AS yline, 'limit' AS label, 'orange' AS color;
